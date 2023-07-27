893440d42df7a72acfedd6cff5ce10346de8932e
Đăng Minh <dangminh2208@gmail.com>
2023-07-27 11:33:51 +0700

    Add notebook

--- src/main.py
+++ src/main.py
@@ -3,14 +3,14 @@ import visualize
 from check import test, check
 from operations import normalized_graph
 
-data = graph.generate(v=10, dim=3, shuffle=False, equidistant=False, write=True)
-data = graph.load(3, False, 10)
+data = graph.generate(v=10, dim=2, shuffle=False, equidistant=False)
+data = graph.load(2, True, 9)
 
 
 check(data.original, data.similar)
 print("\n")
-check(data.original, data.position_modified)
-print("\n")
-check(data.original, data.color_modified)
-print("\n")
-check(data.original, data.edge_removed)
+# check(data.original, data.position_modified)
+# print("\n")
+# check(data.original, data.color_modified)
+# print("\n")
+# check(data.original, data.edge_removed)
